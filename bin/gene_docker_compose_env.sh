f1c68636519c34f91464b02a9e1de77f9d968f61
中田卓郎 <takurou403@gmail.com>
2022-09-09 16:23:34 +0900

    .env自動作成スクリプト更新

--- bin/gene_docker_compose_env.sh
+++ bin/gene_docker_compose_env.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
+#### docker-compse.ymlの.envファイルを生成するスクリプト ####
+
 # .envrcファイルが存在しない場合は新規作成
 if [ ! -f .envrc ]; then
   cat .envrc.example > .envrc
+  echo -e "Created a new .env file for docker-compose. \n Please write the settings in the .env file."
+  return;
 fi
 
+# .envrcを読み込みdocker-compse.ymlの.envファイルを生成する
 source .envrc
 
 PROJECT_ROOT=$(pwd)
@@ -12,18 +17,6 @@ INIT_DIR_PATH="${PROJECT_ROOT}/.devcontainer/db/init"
 INIT_SQLFILE_PATH="${INIT_DIR_PATH}/init.sql"
 DOCKER_ENVFILE_PATH="${PROJECT_ROOT}/.devcontainer/.env"
 DB_DATA_PATH="${PROJECT_ROOT}/.devcontainer/db/data"
-
-# 初回に作成するsqlファイルを格納するディレクトリが存在しない場合
-if [ ! -d $INIT_DIR_PATH ]; then
-  mkdir $INIT_DIR_PATH
-fi
-
-# 初回に作成するsqlファイルが存在しない場合
-if [ ! -f $INIT_SQLFILE_PATH ]; then
-  echo "CREATE DATABASE IF NOT EXISTS ${DB_DATABASE} CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;" > $INIT_SQLFILE_PATH
-  rm -rf "$DB_DATA_PATH"
-fi
-
 
 # docker-composeの.envファイルが存在しない場合は作成
 if [ ! -f $DOCKER_ENVFILE_PATH ]; then
@@ -35,7 +28,18 @@ if [ ! -f $DOCKER_ENVFILE_PATH ]; then
   echo "USER=${USER}" >> $DOCKER_ENVFILE_PATH
   echo "DB_PASSWORD=${DB_PASSWORD}" >> $DOCKER_ENVFILE_PATH
   echo "PROXY_PORT=${PROXY_PORT}" >> $DOCKER_ENVFILE_PATH
+  echo "PHP_MYADMIN_PORT=${PHP_MYADMIN_PORT}" >> $DOCKER_ENVFILE_PATH
   echo "BACKEND_PORT=${BACKEND_PORT}" >> $DOCKER_ENVFILE_PATH
   echo "FRONTEND_PORT=${FRONTEND_PORT}" >> $DOCKER_ENVFILE_PATH
-  echo "PHP_MYADMIN_PORT=${PHP_MYADMIN_PORT}" >> $DOCKER_ENVFILE_PATH
+fi
+
+# 初回に作成するsqlファイルを格納するディレクトリが存在しない場合
+if [ ! -d $INIT_DIR_PATH ]; then
+  mkdir $INIT_DIR_PATH
+fi
+
+# 初回に作成するsqlファイルが存在しない場合
+if [ ! -f $INIT_SQLFILE_PATH ]; then
+  echo "CREATE DATABASE IF NOT EXISTS ${DB_DATABASE} CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;" > $INIT_SQLFILE_PATH
+  rm -rf "$DB_DATA_PATH"
 fi
