4467101f3c7cecf80315126af38e8ec8e9b803ed
takurou <takurou.bhw.useru@gmail.com>
2024-07-20 13:58:25 +0900

    install.sh調整

--- .devcontainer/php/install.sh
+++ .devcontainer/php/install.sh
@@ -2,7 +2,6 @@
 
 echo 'run install.sh ' >> history.log
 # プロジェクトディレクトリが存在しない場合はlaravelを新規インストール
-if [ !-d $1 ]; then
-	composer create-project laravel/laravel "${PROJECT_NAME}" "${LARAVEL_VERSION}" --prefer-dist
+if [ ! -d "/var/www/html/${PROJECT_NAME}" ]; then
+  composer create-project laravel/laravel "$PROJECT_NAME" "$LARAVEL_VERSION" --prefer-dist
 fi
-
